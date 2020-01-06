cd27dfc4d61c285e111c8b6d4dbc291cabc0e5aa
Weidong Zhu <weidong@Weidongs-MacBook-Pro.local>
2020-01-06 09:15:00 -0500

    Update shell script.

--- test_all.sh
+++ test_all.sh
@@ -10,21 +10,14 @@ if [ ! -d "$OUTPUT_FOLDER" ]; then
     mkdir "$OUTPUT_FOLDER"
 fi
 
-sudo bash single_evaluation.sh dedupschems=2 tracetype=1 tracename=homes_500000.trace resultname=${OUTPUT_FOLDER}/homes.sample capacity=100 devicename=/dev/nvme0n1
+sudo bash single_evaluation.sh tracetype=0 tracename=traces/financial.txt resultname=${OUTPUT_FOLDER}/financial.sample capacity=8 devicename=/dev/nvme0n1
 
-sudo bash single_evaluation.sh dedupschems=2 tracetype=2 tracename=web_500000.trace resultname=${OUTPUT_FOLDER}/web.sample capacity=100 devicename=/dev/nvme0n1
+sudo bash single_evaluation.sh tracetype=0 tracename=traces/hm_0.txt resultname=${OUTPUT_FOLDER}/hm_0.sample capacity=8 devicename=/dev/nvme0n1
 
-sudo bash single_evaluation.sh dedupschems=2 tracetype=2 tracename=mail_500000.trace resultname=${OUTPUT_FOLDER}/mail.sample capacity=100 devicename=/dev/nvme0n1
+sudo bash single_evaluation.sh tracetype=0 tracename=traces/prxy_0.txt resultname=${OUTPUT_FOLDER}/prxy_0.sample capacity=8 devicename=/dev/nvme0n1
 
-sudo bash single_evaluation.sh dedupschems=0 tracetype=2 tracename=web_500000.trace resultname=${OUTPUT_FOLDER}/web.traditional capacity=100 devicename=/dev/nvme0n1
+sudo bash single_evaluation.sh tracetype=0 tracename=traces/rsrch_0.txt resultname=${OUTPUT_FOLDER}/rsrch_0.sample capacity=8 devicename=/dev/nvme0n1
 
-sudo bash single_evaluation.sh dedupschems=1 tracetype=2 tracename=web_500000.trace resultname=${OUTPUT_FOLDER}/web.EaD capacity=100 devicename=/dev/nvme0n1
+sudo bash single_evaluation.sh tracetype=0 tracename=traces/wdev_0.txt resultname=${OUTPUT_FOLDER}/wdev_0.sample capacity=8 devicename=/dev/nvme0n1
 
-sudo bash single_evaluation.sh dedupschems=0 tracetype=1 tracename=homes_500000.trace resultname=${OUTPUT_FOLDER}/homes.traditional capacity=100 devicename=/dev/nvme0n1
-
-sudo bash single_evaluation.sh dedupschems=1 tracetype=1 tracename=homes_500000.trace resultname=${OUTPUT_FOLDER}/homes.EaD capacity=100 devicename=/dev/nvme0n1
-
-sudo bash single_evaluation.sh dedupschems=0 tracetype=2 tracename=mail_500000.trace resultname=${OUTPUT_FOLDER}/mail.traditional capacity=100 devicename=/dev/nvme0n1
-
-sudo bash single_evaluation.sh dedupschems=1 tracetype=2 tracename=mail_500000.trace resultname=${OUTPUT_FOLDER}/mail.EaD capacity=100 devicename=/dev/nvme0n1
 
