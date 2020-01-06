58e614763cdb0ed70baa980b1d02ffd2e4e159fe
Weidong Zhu <weidong@Weidongs-MacBook-Pro.local>
2020-01-06 18:42:14 -0500

    Changed the test_all.sh.

--- test_all.sh
+++ test_all.sh
@@ -10,14 +10,14 @@ if [ ! -d "$OUTPUT_FOLDER" ]; then
     mkdir "$OUTPUT_FOLDER"
 fi
 
-sudo bash single_evaluation.sh tracetype=0 tracename=traces/financial.txt resultname=${OUTPUT_FOLDER}/financial.sample capacity=8 devicename=/dev/nvme0n1
+sudo bash single_evaluation.sh tracetype=0 tracename=traces/financial.txt resultname=financial.sample capacity=8 devicename=/dev/nvme0n1
 
-sudo bash single_evaluation.sh tracetype=0 tracename=traces/hm_0.txt resultname=${OUTPUT_FOLDER}/hm_0.sample capacity=8 devicename=/dev/nvme0n1
+sudo bash single_evaluation.sh tracetype=0 tracename=traces/hm_0.txt resultname=hm_0.sample capacity=8 devicename=/dev/nvme0n1
 
-sudo bash single_evaluation.sh tracetype=0 tracename=traces/prxy_0.txt resultname=${OUTPUT_FOLDER}/prxy_0.sample capacity=8 devicename=/dev/nvme0n1
+sudo bash single_evaluation.sh tracetype=0 tracename=traces/prxy_0.txt resultname=prxy_0.sample capacity=8 devicename=/dev/nvme0n1
 
-sudo bash single_evaluation.sh tracetype=0 tracename=traces/rsrch_0.txt resultname=${OUTPUT_FOLDER}/rsrch_0.sample capacity=8 devicename=/dev/nvme0n1
+sudo bash single_evaluation.sh tracetype=0 tracename=traces/rsrch_0.txt resultname=rsrch_0.sample capacity=8 devicename=/dev/nvme0n1
 
-sudo bash single_evaluation.sh tracetype=0 tracename=traces/wdev_0.txt resultname=${OUTPUT_FOLDER}/wdev_0.sample capacity=8 devicename=/dev/nvme0n1
+sudo bash single_evaluation.sh tracetype=0 tracename=traces/wdev_0.txt resultname=wdev_0.sample capacity=8 devicename=/dev/nvme0n1
 
 
