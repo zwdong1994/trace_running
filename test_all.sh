bc633e30f204384f902c0c9f06dc465cb15c1fa7
Weidong Zhu <weidong@ssrb-vpn2-8-193.vpn.ufl.edu>
2020-01-07 13:29:18 -0500

    Modify test_all.

--- test_all.sh
+++ test_all.sh
@@ -10,14 +10,14 @@ if [ ! -d "$OUTPUT_FOLDER" ]; then
     mkdir "$OUTPUT_FOLDER"
 fi
 
-sudo bash single_evaluation.sh tracetype=0 tracename=traces/financial.txt resultname=financial.sample capacity=8 devicename=/dev/nvme0n1
+sudo bash single_evaluation.sh tracetype=0 timescale=10 tracename=traces/financial_100000.txt resultname=financial.sample capacity=8 devicename=/dev/nvme0n1
 
-sudo bash single_evaluation.sh tracetype=0 tracename=traces/hm_0.txt resultname=hm_0.sample capacity=8 devicename=/dev/nvme0n1
+sudo bash single_evaluation.sh tracetype=0 timescale=10 tracename=traces/hm_0_100000.txt resultname=hm_0.sample capacity=8 devicename=/dev/nvme0n1
 
-sudo bash single_evaluation.sh tracetype=0 tracename=traces/prxy_0.txt resultname=prxy_0.sample capacity=8 devicename=/dev/nvme0n1
+sudo bash single_evaluation.sh tracetype=0 timescale=10 tracename=traces/prxy_0_100000.txt resultname=prxy_0.sample capacity=8 devicename=/dev/nvme0n1
 
-sudo bash single_evaluation.sh tracetype=0 tracename=traces/rsrch_0.txt resultname=rsrch_0.sample capacity=8 devicename=/dev/nvme0n1
+sudo bash single_evaluation.sh tracetype=0 timescale=10 tracename=traces/rsrch_0_100000.txt resultname=rsrch_0.sample capacity=8 devicename=/dev/nvme0n1
 
-sudo bash single_evaluation.sh tracetype=0 tracename=traces/wdev_0.txt resultname=wdev_0.sample capacity=8 devicename=/dev/nvme0n1
+sudo bash single_evaluation.sh tracetype=0 timescale=10 tracename=traces/wdev_0_100000.txt resultname=wdev_0.sample capacity=8 devicename=/dev/nvme0n1
 
-sudo bash single_evaluation.sh tracetype=0 tracename=traces/mds_0.txt resultname=mds_0.sample capacity=8 devicename=/dev/nvme0n1
+sudo bash single_evaluation.sh tracetype=0 timescale=10 tracename=traces/mds_0_100000.txt resultname=mds_0.sample capacity=8 devicename=/dev/nvme0n1
